1706304083cbadabfe76c315c224e9090b3d2ca3
Jerom <91853188+JeroMontoya@users.noreply.github.com>
2026-06-27 19:53:35 -0500

    security: remove secrets from repo, add .env to gitignore

--- check_headers.cjs
+++
@@ -1,8 +0,0 @@
-const https = require('https');
-
-https.get('https://apes-tiendanube-crm.vercel.app', (res) => {
-  console.log('Status Code:', res.statusCode);
-  console.log('Headers:', res.headers);
-}).on('error', (e) => {
-  console.error(e);
-});
