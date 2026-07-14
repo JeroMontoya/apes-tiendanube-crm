377406eec6564b490d5db02e65bd719fa459c08f
Jerom <91853188+JeroMontoya@users.noreply.github.com>
2026-07-14 18:40:29 -0500

    feat: add force=1 param to snapshot for regen

--- server/server.js
+++ server/server.js
@@ -1157,7 +1157,8 @@ app.get('/api/data/snapshot', async (req, res) => {
     const sample = unifiedClients[0];
     const rawSample = rawOrders[0];
     const rawOrdersStale = rawSample && rawSample.date && typeof rawSample.date === 'object';
-    const needsRegen = unifiedClients.length > 0 && (
+    const forceRegen = req.query.force === '1';
+    const needsRegen = forceRegen || (unifiedClients.length > 0 && (
       (sample.orders && !sample.purchases) ||
       (sample.purchases?.length > 0 && !sample.purchases[0].date) ||
       (sample.purchases?.length > 0 && !('couponType' in sample.purchases[0])) ||
